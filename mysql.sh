6c6f77e5f8536c091b4212d1d2082569996ba389
nirmala <nirmalamurahari505@gmail.com>
2025-02-23 16:48:02 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 USERID=$(id -u)
-R="\e[32m"
-G="\e[33m"
-Y="\e[34m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
